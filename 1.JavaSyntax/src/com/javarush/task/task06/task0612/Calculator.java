3dd23f8a85f0c0bbf02e4a79a333a617519ccc4e
altervotum <30844680+altervotum@users.noreply.github.com>
2019-05-07 01:06:33 +0300

    new task complete

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0612/Calculator.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task06.task0612;
+
+/* 
+Калькулятор
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        //напишите тут ваш код
+
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        //напишите тут ваш код
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        //напишите тут ваш код
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        //напишите тут ваш код
+        double d = (double)a / b;
+        return d;
+    }
+
+    public static double percent(int a, int b) {
+        //напишите тут ваш код
+        double p = ((double)b * a) / 100;
+        return p;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

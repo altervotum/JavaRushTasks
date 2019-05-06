4542cedd67804e0ef4df041d58533cb6fef6b3bf
altervotum <30844680+altervotum@users.noreply.github.com>
2019-05-07 00:02:15 +0300

    1

--- 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
@@ -3,20 +3,25 @@ package com.javarush.task.task05.task0529;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-/* 
-Консоль-копилка
-*/
+/* Консоль-копилка*/
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
 
         while (true){
             String s = reader.readLine();
 
+
             if (s.equals("сумма")){
-                
+                System.out.println(sum);
+                break;
+            }
+            else {
+                int x = Integer.parseInt(s);
+                sum += x;
             }
 
         }
